44363f8f5ee8bda4e11e33218f34321fc29ebf38
ishita-Pal <143524834+ishita-Pal@users.noreply.github.com>
2024-09-16 06:55:00 +0530

    Update database.py

--- qqqqqqqqq/FastApi/database.py
+++ qqqqqqqqq/FastApi/database.py
@@ -1,8 +1,13 @@
-# database.py
-import os
+# Install python-dotenv if you haven't
+# pip install python-dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from a .env file
 
 URL_DATABASE = os.getenv('DATABASE_URL', 'mysql+pymysql://root:Loveislove12!@localhost:3306/test4')
 
