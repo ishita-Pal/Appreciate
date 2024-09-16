be727d7ec203e9b81d22d35db26955a334f2b04a
ishita-Pal <143524834+ishita-Pal@users.noreply.github.com>
2024-09-16 14:44:33 +0530

    Update login.jsx

--- qqqqqqqqq/my-app/src/Login/login.jsx
+++ qqqqqqqqq/my-app/src/Login/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:8000/login/', {
+            const response = await axios.post('https://appreciate-12.onrender.com/login/', {
                 username,
                 password,
             });
