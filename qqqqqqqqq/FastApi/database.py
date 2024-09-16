e302fbbab3ce5b7fc9910ca6bcb783ba0b3deb3e
ishita-Pal <143524834+ishita-Pal@users.noreply.github.com>
2024-09-16 06:45:11 +0530

    Update database.py

--- qqqqqqqqq/FastApi/database.py
+++ qqqqqqqqq/FastApi/database.py
@@ -1,11 +1,11 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-# URL_DATABASE = 'mysql+pymysql://root:Loveislove12!@localhost:3306/test4'
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Loveislove12!@localhost:3306/test4'
 
+URL_DATABASE = 'mysql+pymysql://root:Loveislove12!@localhost:3306/test4'
 
 engine = create_engine(URL_DATABASE)
-SessionLocal = sessionmaker(autocommit=False , autoflush=False , bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base=declarative_base()
+Base = declarative_base()
