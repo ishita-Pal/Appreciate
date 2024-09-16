0f14ba2ef48949de1707bd67b84f0a0cf3e52e81
ishita-Pal <143524834+ishita-Pal@users.noreply.github.com>
2024-09-16 14:22:38 +0530

    Update database.py

--- qqqqqqqqq/FastApi/database.py
+++ qqqqqqqqq/FastApi/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-DATABASE_URL = "mysql://avnadmin:AVNS_d4nhudYtpjbNGhWJYAQ@mysql-8e18851-ishitapal1828-ffa7.c.aivencloud.com:11246/test4"
-engine = create_engine(URL_DATABASE)
-SessionLocal = sessionmaker(autocommit=False , autoflush=False , bind=engine)
 
-Base=declarative_base()
+# Use the correct variable name DATABASE_URL
+DATABASE_URL = "mysql://avnadmin:AVNS_d4nhudYtpjbNGhWJYAQ@mysql-8e18851-ishitapal1828-ffa7.c.aivencloud.com:11246/test4"
+
+# Create engine using DATABASE_URL
+engine = create_engine(DATABASE_URL)
+
+# SessionLocal will be used to interact with the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for all models
+Base = declarative_base()
